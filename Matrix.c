842f67563e4907d275c2c7a831272dde536be00a
Sonia Atre <atresonia@gmail.com>
2019-10-20 13:17:06 -0700

    finish changeEntry

--- Matrix.c
+++ Matrix.c
@@ -9,11 +9,12 @@
 *Role: creates a Matrix ADT used for the Matrix implementation
 */
 
-#include<stdlib.h>
-#include<stdio.h>
-#include"List.h"
-#include<assert.h>
-#include"Matrix.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include "List.h"
+#include "Matrix.h"
 
 // Private Types and Functions ------------------------------------------------
 //EntryObj type
@@ -28,7 +29,7 @@ typedef EntryObj* Entry;
 typedef struct MatrixObj {
 	int size;
 	List* entries;
-	int NZE;
+	int NNZ;
 } MatrixObj;
 
 //newEntry()
@@ -45,13 +46,21 @@ Entry newEntry(int c, double v) {
 //returns M to empty state
 void clearM(Matrix M) {
 	if (M == NULL) {
-		fprintf(stderr, "Matrix Error: clear() called on NULL Matrix reference\n");
+		fprintf(stderr, "Matrix Error: clearM() called on NULL Matrix reference\n");
 		exit(EXIT_FAILURE);
 	}
 	for (int i = 0; i < size(M); i++) {
 		List L = (M->entries)[i];
 		clear(L);
 	}
+}
+
+int isNearlyEqual(double a, double b) {
+	double epsilon = 0.0001;
+	if (fabs(a - b) < epsilon) {
+		return 1;
+	}
+	return 0;
 }
 
 //freeEntry()
@@ -82,13 +91,21 @@ Matrix newMatrix(int n) {
 //freeList()
 //Destructor for the List ADT
 void freeMatrix(Matrix* pM) {
-	int i = 0;
-	if (pM != NULL && *pM != NULL) {	
+	if (pM != NULL && *pM != NULL) {
+		for (int i = 0; i < (*pM)->size; i++) {
+			if ((*pM)->entries[i] != NULL) {
+				//clear((*pM)->entries[i]);
+				List L = (*pM)->entries[i];
+				for (moveFront(L); index(L) >= 0; moveNext(L)) {
+					free((Entry)get(L));
+				}
+				freeList(&L);
+			}
+		}
+
 		free((*pM)->entries);
-		//clearM(*pM);
 		free(*pM);
 		*pM = NULL;
-		i++;
 	}
 }
 
@@ -107,10 +124,10 @@ int size(Matrix M) {
 // Return the number of non-zero elements in M.
 int NNZ(Matrix M) {
 	if (M == NULL) {
-		fprintf(stderr, "Matrix Error: size() called on NULL Matrix reference\n");
+		fprintf(stderr, "Matrix Error: NNZ() called on NULL Matrix reference\n");
 		exit(EXIT_FAILURE);
 	}
-	return M->NZE;
+	return M->NNZ;
 
 }
 /*
@@ -138,7 +155,7 @@ void makeZero(Matrix M) {
 
 void changeEntry(Matrix M, int i, int j, double x) {
 	if (M == NULL) {
-		fprintf(stderr, "Matrix Error: size() called on NULL Matrix reference\n");
+		fprintf(stderr, "Matrix Error: changeEntry() called on NULL Matrix reference\n");
 		exit(EXIT_FAILURE);
 	}
 	if (i < 1 || i > size(M)) {
@@ -146,113 +163,115 @@ void changeEntry(Matrix M, int i, int j, double x) {
 		exit(EXIT_FAILURE);
 	}
 	if (j < 1 || j > size(M)) {
-		fprintf(stderr, "Invalid row size for j\n");
+		fprintf(stderr, "Invalid column size for j\n");
 		exit(EXIT_FAILURE);
 	}
-	Entry E = newEntry(j, x);
-	List L = (M->entries)[i];
-	//if List hasn't been created yet
-	if (L == NULL) {
-		L = newList();
+
+	if (isNearlyEqual(x, 0.0) && M->entries[i - 1] == NULL) {
+		return;
 	}
-	//list empty
-	if (index(L) == -1 && x != 0) {
-		append(L, E);
-		M->NZE++;
-	} else {
-		while (index(L) != -1) {
-			moveFront(L);
-			Entry P = (Entry)get(L);
-			//if cursor is on entry, delete or change entry
-			if (P->column == j) {
-				if (x == 0) {
-					delete(L);
-					M->NZE--;
-				} else {
-					P->value = x;
-				}
-			}//cursor is before or after entry to add
-			else if (P->column > j && x != 0) {
-				insertBefore(L,  E);
-				M->NZE++;
-			} else if (P->column < j && x != 0) {
-				insertAfter(L, E);
-				M->NZE++;
+
+	//if List hasn't been created yet
+	if (M->entries[i - 1] == NULL) {
+		M->entries[i - 1] = newList();
+		Entry E = newEntry(j, x);
+		append(M->entries[i - 1], E);
+		return;
+	}
+
+	//see if entry already exists
+	List L = M->entries[i - 1];
+	for (moveFront(L); index(L) >= 0; moveNext(L)) {
+		Entry E = (Entry)get(L);
+		if (E->column == j) {
+			if (isNearlyEqual(x, 0.0)) {
+				delete(L);
+			} else {
+				//modify existing entry
+				E->value = x;
 			}
+			return;
+		} else if (E->column > j && !isNearlyEqual(x, 0.0)) {
+			insertBefore(L, newEntry(j, x));
+			return;
 		}
 	}
-	
+
+	//inserting last entry
+	if (!isNearlyEqual(x, 0.0)) {
+		append(L, newEntry(j, x));
+	}
 }
 
 
 
 // Matrix Arithmetic operations ------------------------------------------------
-		/*
-		// copy()
-		// Returns a reference to a new Matrix object having the same entries as A.
-		Matrix copy(Matrix A) {
+/*
+// copy()
+// Returns a reference to a new Matrix object having the same entries as A.
+Matrix copy(Matrix A) {
 
-		}
-		// transpose()
-		// Returns a reference to a new Matrix object representing the transpose
-		// of A.
-		Matrix transpose(Matrix A) {
+}
+// transpose()
+// Returns a reference to a new Matrix object representing the transpose
+// of A.
+Matrix transpose(Matrix A) {
 
-		}
-		// scalarMult()
-		// Returns a reference to a new Matrix object representing xA.
-		Matrix scalarMult(double x, Matrix A) {
+}
+// scalarMult()
+// Returns a reference to a new Matrix object representing xA.
+Matrix scalarMult(double x, Matrix A) {
 
-		}
-		// sum()
-		// Returns a reference to a new Matrix object representing A+B.
-		// pre: size(A)==size(B)
-		Matrix sum(Matrix A, Matrix B) {
+}
+// sum()
+// Returns a reference to a new Matrix object representing A+B.
+// pre: size(A)==size(B)
+Matrix sum(Matrix A, Matrix B) {
 
-		}
-		// diff()
-		// Returns a reference to a new Matrix object representing A-B.
-		// pre: size(A)==size(B)
-		Matrix diff(Matrix A, Matrix B) {
+}
+// diff()
+// Returns a reference to a new Matrix object representing A-B.
+// pre: size(A)==size(B)
+Matrix diff(Matrix A, Matrix B) {
 
-		}
-		// product()
-		// Returns a reference to a new Matrix object representing AB
-		// pre: size(A)==size(B)
-		Matrix product(Matrix A, Matrix B) {
+}
+// product()
+// Returns a reference to a new Matrix object representing AB
+// pre: size(A)==size(B)
+Matrix product(Matrix A, Matrix B) {
 
-		}
-		*/
+}
+*/
 // printMatrix()
 // Prints a string representation of Matrix M to filestream out. Zero rows
 // are not printed. Each non-zero is represented as one line consisting
 // of the row number, followed by a colon, a space, then a space separated
 // list of pairs "(col, val)" giving the column numbers and non-zero values
 // in that row. The double val will be rounded to 1 decimal point.
-		/*
-		void printMatrix(FILE* out, Matrix M) {
-			if (M == NULL) {
-				fprintf(stderr, "Matrix Error: printMatrix() called on NULL Matrix reference\n");
-				exit(EXIT_FAILURE);
-			}
-			for (int i = 1; i <= size(M); i++) {
-				List L = M->entries[i];
-				//fprintf(out, "Index: %d, element val: %f\n", index(L), ((Entry)get(L))->value);
-				if (L != NULL) {
-					fprintf(out, "%d: ", i);
-					moveFront(L);
-					fprintf(out, "Index: %d, ", length(L));
+/*
+void printMatrix(FILE* out, Matrix M) {
+	if (M == NULL) {
+		fprintf(stderr, "Matrix Error: printMatrix() called on NULL Matrix reference\n");
+		exit(EXIT_FAILURE);
+	}
+	for (int i = 1; i <= size(M); i++) {
+		List L = M->entries[i];
+		//fprintf(out, "Index: %d, element val: %f\n", index(L), ((Entry)get(L))->value);
+		if (L != NULL) {
+			fprintf(out, "%d: ", i);
+			moveFront(L);
+			fprintf(out, "Index: %d, ", length(L));
 
-					for (moveFront(L); index(L) >= 0; moveNext(L)) {
-						Entry entry = (Entry)get(L);
-						fprintf(out, "(%d, %f) ", entry->column, entry->value);
-
-					}
-				}
+			for (moveFront(L); index(L) >= 0; moveNext(L)) {
+				Entry entry = (Entry)get(L);
+				fprintf(out, "(%d, %f) ", entry->column, entry->value);
 
 			}
 		}
-		*/
+
+	}
+}
+*/
 
 
 
